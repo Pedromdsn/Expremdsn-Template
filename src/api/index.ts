e4c44fad0cde542063f6c545a690a6d93f310564
pedromdsn <pedromdsn.2002@hotmail.com>
2022-02-07 03:45:19 +0000

    feat: :see_no_evil: env

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { GetMethod } from '@/lib'
-
-export const getMethod: GetMethod = async (req, res) => {
-  res.send('Hello World!')
-}
--- tests/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import supertest from 'supertest'
-import { app } from '@/app'
-
-describe('Test the root path', () => {
-  test('It should response the GET method', () => {
-    return supertest(app).get('/').expect(200)
-  })
-})
