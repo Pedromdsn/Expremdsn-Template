30628aae4a0cf7c704c5edf8bd7565f69b6e6ccf
pedromdsn <pedromdsn.2002@hotmail.com>
2022-02-07 03:45:36 +0000

    feat: :see_no_evil: env

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { loadAllFile } from '@/libs/lib/lib'
+
+const app = Router()
+
+loadAllFile('./src/api')
+
+// app.get('/teste/:id', (req, res) => {
+//   const { id } = req.params
+//   res.send(`Hello World! ${id}`)
+// })
+
+export { app as router }
