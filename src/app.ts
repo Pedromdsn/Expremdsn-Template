e525af07f4c9d2b24c92b82dbb9687cea69686eb
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-03-08 02:47:44 +0000

    feat: :ambulance: Expremdsn import fix

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import expremdsn from 'expremdsn'
 import cors from 'cors'
 
 import { router } from 'expremdsn'
 
-const app = express()
+const app = expremdsn()
 
-app.use(express.json())
+app.use(expremdsn.json())
 app.use(cors())
 app.use(router)
 
